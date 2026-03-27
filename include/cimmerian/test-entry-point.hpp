b6f5bba7ebc3ae0ef041842eeaa0da068f84d326
Dean Wilson <mail@deanwilson.dev>
2026-03-27 18:08:52 +0800

    fixup readme

--- include/cimmerian/test-entry-point.hpp
+++ include/cimmerian/test-entry-point.hpp
@@ -6,11 +6,8 @@
 int main(int argc, char* argv[])
 {
   Cimmerian::CheckDebug(argc, argv);
-  Cimmerian::TestRunner* runner = new Cimmerian::TestRunner();
-  Cimmerian::TestRegistry& registry = Cimmerian::TestRegistry().GetInstance();
-  Cimmerian::TestRunSummary summary = runner->RunAll(&registry);
-  delete runner;
-  runner = nullptr;
+  Cimmerian::TestRunner runner = Cimmerian::TestRunner();
+  Cimmerian::TestRunSummary summary = runner.RunAll(&Cimmerian::TestRegistry().GetInstance());
   return (summary.failed == 0) ? 0 : 1;
   return 0;
 }
